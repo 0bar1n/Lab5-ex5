7de529ff9fab03bd1b69c7637dd5dd68f4f307d9
barinskaya <1388zxc@gmail.com>
2022-09-30 15:31:01 +0300

    Добавьте файлы проекта.

---
+++ Практическая 5 пример 5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");// просим пользователя ввести данные
+            string name = Console.ReadLine(); //ввод переменной строкового типа
+            Console.WriteLine($"Привет {name}");//вывод переменной
+            Console.ReadKey(); //остановка выполнения программы до тех пор, пока пользователь не нажмет клавишу, завершение работы
+        }
+    }
+}
